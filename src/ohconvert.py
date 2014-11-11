ca050c06c28446bed4d653103efac63a5801363f
Sylvain Viollon <thefunny@gmail.com>
2014-11-11 15:30:15 +0100

    Update code to work with Python 3.4 as well.

--- src/ohconvert.py
+++ src/ohconvert.py
@@ -11,6 +11,13 @@ import sys
 from optparse import OptionParser
 from subprocess import Popen, PIPE
 
+
+def execute(*cmd):
+    process = Popen(cmd, bufsize=-1, universal_newlines=True, stdout=PIPE)
+    stdout, stderr = process.communicate()
+    return stdout
+
+
 def count(names):
     """Create ouptut equivalent to::
     sloccount --duplicates --wide --details path ...
@@ -21,7 +28,8 @@ def count(names):
         yield "WARNING!!! Not a file nor a directory (so ignored): %s" % name
     if len(names) == 1:
         directory = os.path.abspath(names[0])
-        paths = [os.path.join(directory, name) for name in os.listdir(directory) if not name.startswith('.')]
+        paths = [os.path.join(directory, name)
+                 for name in os.listdir(directory) if not name.startswith('.')]
     else:
         paths = [os.path.abspath(name) for name in names]
     directories = [path for path in paths if os.path.isdir(path)]
@@ -40,13 +48,15 @@ def count(names):
         for output in count_directory(directory):
             yield output
 
+
 def count_directory(directory):
     directoryname = os.path.basename(directory)
-    stdout, stderr = Popen(['ohcount', '-d', directory], stdout=PIPE).communicate()
+    stdout = execute('ohcount', '-d', directory)
     lines = (line.split('\t') for line in stdout.split('\n')[:-1])
     paths = (path for lang, path in lines if lang != '(null)')
     for output in count_paths(directoryname, paths):
         yield output
+
 
 def count_paths(directoryname, paths):
     for path in paths:
@@ -54,17 +64,20 @@ def count_paths(directoryname, paths):
         if output is not None:
             yield output
 
+
 def ohcount_file(path, directoryname):
-        stdout, stderr = Popen(['ohcount', '-i', path], stdout=PIPE).communicate()
+        stdout = execute('ohcount', '-i', path)
         result = stdout.split('\n')[4]
         if result:
             lang, code = result.split()[:2]
             return '\t'.join((code, lang, directoryname, path))
 
+
 def add_newlines(lines):
     for line in lines:
         yield line
         yield '\n'
+
 
 def main():
     parser = OptionParser(usage=usage)
